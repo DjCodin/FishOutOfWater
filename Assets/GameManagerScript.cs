217d8200ba8d10eb9bd890f2d30d6c24c7802d8c
Everett Eng <everetteng07@gmail.com>
2025-01-16 18:39:23 -0500

    Update 1/16
    - Added a working opponent for day 1 tic tac toe
    - Implemented images from the art team

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Linq;
 
 public class GameManagerScript : MonoBehaviour
 {
@@ -22,7 +23,14 @@ public class GameManagerScript : MonoBehaviour
     public GameObject x;
     public GameObject o;
     public bool instantiated = false;
-
+    public bool isX = false;
+    public bool isO = false;
+    public GameObject board;
+    public bool playerTurn;
+    public bool gameOver = false;
+    public ArrayList buttons;
+    public bool buttonClicked = false;
+    public float timer2 = 0;
     void Start()
     {
         infoText.text = "Your shape is:";
@@ -38,10 +46,31 @@ public class GameManagerScript : MonoBehaviour
         button7.gameObject.SetActive(false);
         button8.gameObject.SetActive(false);
         button9.gameObject.SetActive(false);
+        board = GameObject.FindGameObjectWithTag("Board");
+        // Hide the buttons
+        board.SetActive(false);
+
+        // Create arraylist with all the buttons
+        buttons = new ArrayList();
+        buttons.Add(button1);
+        buttons.Add(button2);
+        buttons.Add(button3);
+        buttons.Add(button4);
+        buttons.Add(button5);
+        buttons.Add(button6);
+        buttons.Add(button7);
+        buttons.Add(button8);
+        buttons.Add(button9);
+
     }
 
     void Update()
     {
+
+        foreach (Button btn in buttons)
+        {
+            btn.onClick.AddListener(() => HandleButtonClick(btn));
+        }
         // Cutscene script 
         if (!cutsceneDone)
         {
@@ -49,18 +78,22 @@ public class GameManagerScript : MonoBehaviour
             if (timer >= 2 && !instantiated)
             {
                 timer = 0;
-                int randInt = Random.Range(0, 2);
+                int randInt = Random.Range(0, 1);
                 if (randInt == 0)
                 {
                     var copy = Instantiate(x, new Vector2(0, 0), Quaternion.identity);
                     Destroy(copy, 3.0f);
                     instantiated = true;
+                    isX = true;
+                    playerTurn = true;
                 }
                 else
                 {
                     var copy = Instantiate(o, new Vector2(0, 0), Quaternion.identity);
                     Destroy(copy, 3.0f);
                     instantiated = true;
+                    isO = true;
+                    playerTurn = false;
 
                 }
             }
@@ -68,6 +101,8 @@ public class GameManagerScript : MonoBehaviour
             {
                 cutsceneDone = true;
                 infoText.text = "";
+                infoText.gameObject.SetActive(false);
+                board.SetActive(true);
             }
         }
         if (cutsceneDone)
@@ -82,8 +117,109 @@ public class GameManagerScript : MonoBehaviour
             button7.gameObject.SetActive(true);
             button8.gameObject.SetActive(true);
             button9.gameObject.SetActive(true);
-
-
         }
+
+        if (isX && !gameOver)
+        {
+            if (playerTurn)
+            {
+                for (int i = 0; i < buttons.Count; i++)
+                {
+                    Button button = (Button)buttons[i];
+                    button.interactable = true;
+                }
+            }
+            if(!playerTurn)
+            {
+                for(int i = 0; i < buttons.Count; i++)
+                {
+                    Button button = (Button) buttons[i];
+                    button.interactable = false;
+                }
+                opponentTurn();
+            }
+            checkForWin();
+            
+        }
+    }
+
+    void opponentTurn()
+    {   
+        timer2 += Time.deltaTime;
+        if (timer2 >= 2f)
+        {
+            int randomSquare = Random.Range(0, buttons.Count);
+            Button button = (Button) buttons[randomSquare];
+            var text = button.GetComponentInChildren<TMP_Text>();
+            int index = 0;
+            if (isX)
+            {
+                text.text = "0";
+                button.interactable = false;
+
+                for (int i = 0; i < buttons.Count; i++)
+                {
+                    if (object.ReferenceEquals(buttons[i], button))
+                    {
+                        index = i;
+                    }
+                }
+                buttons.RemoveAt(index);
+                playerTurn = true;
+            }
+            else
+            {
+                text.text = "X";
+                button.interactable = false;
+
+                for (int i = 0; i < buttons.Count; i++)
+                {
+                    if (object.ReferenceEquals(buttons[i], button))
+                    {
+                        index = i;
+                    }
+                }
+                buttons.RemoveAt(index);
+                playerTurn = true;  
+            }
+            timer2 = 0;
+        }  
+    }
+
+    void checkForWin()
+    {
+
+    }
+
+    void HandleButtonClick(Button clickedButton)
+    {
+        // Make the clicked button not interactable
+        if (buttons.Contains(clickedButton))
+        {
+            int index = 0;
+            clickedButton.interactable = false;
+
+            if (isO)
+            {
+                var buttonText = clickedButton.GetComponentInChildren<TMP_Text>();
+                buttonText.text = "O";
+            }
+            else
+            {
+                var buttonText = clickedButton.GetComponentInChildren<TMP_Text>();
+                buttonText.text = "X";
+            }
+
+            for(int i  = 0; i < buttons.Count; i++)
+            {
+                if (object.ReferenceEquals(buttons[i], clickedButton))
+                {
+                    index = i;
+                }
+            }
+            playerTurn = false;
+            buttons.RemoveAt(index);
+        }
+        
     }
 }
