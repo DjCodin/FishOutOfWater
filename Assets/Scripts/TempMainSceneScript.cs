4bd5c133a305caa0df6b374941205e3ac0349ae0
Everett Eng <everetteng07@gmail.com>
2025-02-13 16:40:56 -0500

    added functionality in the main scene for the mathclass scene and also added a restart key for tic tac toe (r)

--- Assets/Scripts/TempMainSceneScript.cs
+++ Assets/Scripts/TempMainSceneScript.cs
@@ -7,10 +7,12 @@ using UnityEngine.UI;
 public class TempMainSceneScript : MonoBehaviour
 {
     public Button ticTacToeButton;
+    public Button mathClassButton;
     // Start is called before the first frame update
     void Start()
     {
-        ticTacToeButton.onClick.AddListener(() => test(ticTacToeButton));
+        ticTacToeButton.onClick.AddListener(() => loadTicTacToe(ticTacToeButton));
+        mathClassButton.onClick.AddListener(() => loadMathClass(mathClassButton));
     }
 
     // Update is called once per frame
@@ -19,8 +21,13 @@ public class TempMainSceneScript : MonoBehaviour
         
     }
 
-    void test(Button btn)
+    void loadTicTacToe(Button btn)
     {
         SceneManager.LoadScene("TicTacToe");
+    }
+
+    void loadMathClass(Button btn)
+    {
+        SceneManager.LoadScene("MathClass");
     }
 }
