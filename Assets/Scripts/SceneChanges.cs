5cd0baefc981197db694d98ebba0af36672111d9
DjCodin <cdjuensie@gmail.com>
2025-02-15 14:03:32 -0500

    game manager added to math scene

--- Assets/Scripts/SceneChanges.cs
+++ Assets/Scripts/SceneChanges.cs
@@ -14,6 +14,10 @@ public class SceneChanges : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("MainScene");
+        }
         
     }
 
