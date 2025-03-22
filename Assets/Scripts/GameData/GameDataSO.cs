4553928dc075984342452cfd4626fdaa2ec6a17e
Kirai <kaioxley.sia@gmail.com>
2025-03-22 15:09:42 -0400

    Added variables for the academic and social points

--- Assets/Scripts/GameData/GameDataSO.cs
+++ Assets/Scripts/GameData/GameDataSO.cs
@@ -8,10 +8,29 @@ public class GameDataSO : ScriptableObject
     [SerializeField]
     private int _gameDay;
 
+    private int academicScore;
+    private int socialScore;
+
+
+
+
+
     public int GameDay
     {
         get { return _gameDay; }
         set { _gameDay = value; }
     }
 
+
+    public int AcademicScores
+    {
+        get { return academicScore; }
+        set { academicScore = value; }
+    }
+
+    public int SocialScores
+    {
+        get { return socialScore;  }
+        set { socialScore = value; }
+    }
 }
