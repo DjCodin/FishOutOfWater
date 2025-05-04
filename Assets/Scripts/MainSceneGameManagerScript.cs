296111e98145c80a7a67843565f3ec6dca9e7978
DjCodin <cdjuensie@gmail.com>
2025-05-04 11:45:40 -0400

    update

--- Assets/Scripts/MainSceneGameManagerScript.cs
+++ Assets/Scripts/MainSceneGameManagerScript.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainSceneGameManagerScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("MainScene");
+        }
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("MainScene");
+        }
         
     }
 }
